262d8f5d2eec68d64e3db07eb4989c09e482cc00
Bruna Alana <brunna.alanna@gmail.com>
2025-02-26 22:12:55 -0300

    Initial commit

---
+++ src/main/java/brunaalana/dio/decolatech_2025/Application.java
@@ -0,0 +1,13 @@
+package brunaalana.dio.decolatech_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/brunaalana/dio/decolatech_2025/Decolatech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package brunaalana.dio.decolatech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Decolatech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
